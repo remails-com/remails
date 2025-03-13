cdf6c0c2b74acd9d2a4e8147da601d670051ba4d
Maximilian Pohl <maximilian@tweedegolf.com>
2025-03-13 15:29:40 +0100

    Add CRUD organization API endpoints

--- migrations/20240930143937_initial.sql
+++ migrations/20240930143937_initial.sql
@@ -9,16 +9,76 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
+CREATE TABLE organizations
+(
+    id         uuid PRIMARY KEY,
+    name       varchar                  NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at timestamp with time zone NOT NULL DEFAULT now()
+);
+CREATE TRIGGER update_organizations_updated_at
+    BEFORE UPDATE
+    ON organizations
+    FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
+
+
+CREATE TYPE dkim_key_type AS ENUM (
+    'rsa_sha256',
+    'ed25519'
+    );
+
+CREATE TABLE domains
+(
+    domain          varchar PRIMARY KEY,
+    organization_id uuid REFERENCES organizations (id) ON DELETE CASCADE,
+    dkim_key_type   dkim_key_type,
+    dkim_pkcs8_der  bytea,
+    created_at      timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at      timestamp with time zone NOT NULL DEFAULT now()
+);
+
+CREATE TRIGGER update_domains_updated_at
+    BEFORE UPDATE
+    ON domains
+    FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
+
+
+CREATE TABLE api_users
+(
+    id             uuid PRIMARY KEY,
+    roles          jsonb,
+    github_user_id bigint,
+    created_at     timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at     timestamp with time zone NOT NULL DEFAULT now()
+);
+CREATE TRIGGER update_api_users_updated_at
+    BEFORE UPDATE
+    ON api_users
+    FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
+
+CREATE TABLE api_users_organizations
+(
+    api_user_id     uuid                     NOT NULL REFERENCES api_users (id) ON DELETE CASCADE,
+    organization_id uuid                     NOT NULL REFERENCES organizations (id) ON DELETE CASCADE,
+    created_at      timestamp with time zone NOT NULL DEFAULT now(),
+    PRIMARY KEY (api_user_id, organization_id)
+);
+
+
 CREATE TABLE smtp_credential
 (
     id            uuid PRIMARY KEY         NOT NULL,
+    domain        varchar                  NOT NULL REFERENCES domains (domain),
     username      varchar                  NOT NULL UNIQUE CHECK (username ~ '^[a-zA-Z0-9_-]{2,128}$'),
     password_hash varchar                  NOT NULL,
     created_at    timestamp with time zone NOT NULL DEFAULT now(),
     updated_at    timestamp with time zone NOT NULL DEFAULT now()
 );
 
-CREATE TRIGGER update_users_updated_at
+CREATE TRIGGER update_smtp_credential_updated_at
     BEFORE UPDATE
     ON smtp_credential
     FOR EACH ROW
---
+++ src/fixtures/organizations.sql
@@ -0,0 +1,3 @@
+INSERT INTO organizations (id, name)
+VALUES ('44729d9f-a7dc-4226-b412-36a7537f5176', 'test org 1'),
+       ('5d55aec5-136a-407c-952f-5348d4398204', 'test org 2');
