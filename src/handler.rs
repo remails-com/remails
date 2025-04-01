947ff786c4a5ec3ef16b91661e5a9a553ec3ad5d
Marc Schoolderman <marc@tweedegolf.com>
2025-04-01 11:49:26 +0200

    Add fixme

--- src/handler.rs
+++ src/handler.rs
@@ -134,6 +134,9 @@ impl Handler {
                 }
             };
 
+            // TODO FIXME: since messages can be rather large, this clone can have a negative impact on the performance
+            // if the server gets under stress; and it can probably be fixed by making "update_message_status" more efficient
+            // (does it really need to UPDATE the message body simply for setting the status?)
             if let Err(e) = client.send(message.clone()).await {
                 error!("failed to send message: {e}");
 
