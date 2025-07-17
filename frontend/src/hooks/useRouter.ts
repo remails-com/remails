c6674a11b4cfc3aec9145d6fa338b8049dacce55
Marlon Baeten <marlon@tweedegolf.com>
2025-07-17 11:06:32 +0200

    Fix browser history pop stale state

--- frontend/src/hooks/useRouter.ts
+++ frontend/src/hooks/useRouter.ts
@@ -1,5 +1,5 @@
 import { nprogress } from "@mantine/nprogress";
-import { Dispatch, useEffect, useRef } from "react";
+import { Dispatch, useCallback, useEffect, useRef } from "react";
 import { FullRouterState, RouteParams, Router, RouterState } from "../router";
 import { RouteName } from "../routes";
 import { Action, State } from "../types";
@@ -20,64 +20,68 @@ export function useRouter(router: Router, state: State, dispatch: Dispatch<Actio
   const busy = useRef(false);
 
   // Navigate function to change the route
-  const navigate = async (name: RouteName, params?: RouteParams, pushState = true) => {
-    if (busy.current) {
-      console.warn("Navigation is already in progress, ignoring new request.");
-      return false;
-    }
-
-    busy.current = true;
-    let routerState;
-    try {
-      routerState = router.navigate(name, params || {});
-    } catch (e) {
-      console.error(e);
-      routerState = router.navigate("not_found", {});
-    }
-
-    dispatch({
-      type: "set_next_router_state",
-      nextRouterState: routerState,
-    });
-
-    setTimeout(() => {
+  const navigate = useCallback(
+    async (name: RouteName, params?: RouteParams, pushState = true) => {
       if (busy.current) {
-        nprogress.start();
+        console.warn("Navigation is already in progress, ignoring new request.");
+        return false;
       }
-    }, 100);
 
-    const navState: NavigationState = {
-      from: state.routerState,
-      to: routerState,
-      state,
-    };
-
-    for (const mw of middleware) {
+      busy.current = true;
+      let routerState;
       try {
-        routerState = await mw(navState, router, dispatch);
+        routerState = router.navigate(name, params || {});
       } catch (e) {
         console.error(e);
         routerState = router.navigate("not_found", {});
       }
-    }
 
-    if (pushState) {
-      window.history.pushState({ routerState }, "", routerState.fullPath);
-    }
+      dispatch({
+        type: "set_next_router_state",
+        nextRouterState: routerState,
+      });
 
-    dispatch({
-      type: "set_route",
-      routerState: {
-        name: routerState.name,
-        params: routerState.params,
-      },
-    });
+      setTimeout(() => {
+        if (busy.current) {
+          nprogress.start();
+        }
+      }, 100);
 
-    busy.current = false;
-    nprogress.complete();
+      const navState: NavigationState = {
+        from: state.routerState,
+        to: routerState,
+        state,
+      };
 
-    return true;
-  };
+      for (const mw of middleware) {
+        try {
+          routerState = await mw(navState, router, dispatch);
+        } catch (e) {
+          console.error(e);
+          routerState = router.navigate("not_found", {});
+        }
+      }
+
+      if (pushState) {
+        window.history.pushState({ routerState }, "", routerState.fullPath);
+      }
+
+      dispatch({
+        type: "set_route",
+        routerState: {
+          name: routerState.name,
+          params: routerState.params,
+        },
+      });
+
+      busy.current = false;
+      nprogress.complete();
+
+      return true;
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [state.routerState, dispatch, router, middleware]
+  );
 
   // handle back / forward events
   useEffect(() => {
@@ -94,9 +98,7 @@ export function useRouter(router: Router, state: State, dispatch: Dispatch<Actio
     return () => {
       window.removeEventListener("popstate", onPopState);
     };
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dispatch, router]);
+  }, [dispatch, router, navigate]);
 
   return { navigate };
 }
