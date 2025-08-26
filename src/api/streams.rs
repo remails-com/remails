80fd84605e607554fdaf2ae3bceda99f15998a5f
Michiel <michielp1807@gmail.com>
2025-08-26 15:20:29 +0200

    Add unit tests for streams api

--- src/api/streams.rs
+++ src/api/streams.rs
@@ -114,3 +114,167 @@ pub async fn remove_stream(
 
     Ok(Json(stream_id))
 }
+
+#[cfg(test)]
+mod tests {
+    use sqlx::PgPool;
+
+    use crate::api::tests::{TestServer, deserialize_body, serialize_body};
+
+    use super::*;
+
+    #[sqlx::test(fixtures(
+        path = "../fixtures",
+        scripts("organizations", "api_users", "projects")
+    ))]
+    async fn test_stream_lifecycle(pool: PgPool) {
+        let user_a = "9244a050-7d72-451a-9248-4b43d5108235".parse().unwrap(); // is admin of org 1 and 2
+        let org_1 = "44729d9f-a7dc-4226-b412-36a7537f5176";
+        let proj_1 = "3ba14adf-4de1-4fb6-8c20-50cc2ded5462"; // project 1 in org 1
+        let server = TestServer::new(pool.clone(), user_a).await;
+
+        // start without streams
+        let response = server
+            .get(format!(
+                "/api/organizations/{org_1}/projects/{proj_1}/streams"
+            ))
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let streams: Vec<Stream> = deserialize_body(response.into_body()).await;
+        assert_eq!(streams.len(), 0);
+
+        // create a new stream
+        let response = server
+            .post(
+                format!("/api/organizations/{org_1}/projects/{proj_1}/streams"),
+                serialize_body(NewStream {
+                    name: "Test Stream".to_string(),
+                }),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::CREATED);
+        let created_stream: Stream = deserialize_body(response.into_body()).await;
+        assert_eq!(created_stream.name, "Test Stream");
+
+        // list streams
+        let response = server
+            .get(format!(
+                "/api/organizations/{org_1}/projects/{proj_1}/streams"
+            ))
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let streams: Vec<Stream> = deserialize_body(response.into_body()).await;
+        assert_eq!(streams.len(), 1);
+        assert_eq!(streams[0].name, "Test Stream");
+        assert_eq!(streams[0].id(), created_stream.id());
+
+        // update stream
+        let response = server
+            .put(
+                format!(
+                    "/api/organizations/{org_1}/projects/{proj_1}/streams/{}",
+                    created_stream.id()
+                ),
+                serialize_body(NewStream {
+                    name: "Updated Stream".to_string(),
+                }),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let stream: Stream = deserialize_body(response.into_body()).await;
+        assert_eq!(stream.name, "Updated Stream");
+        assert_eq!(stream.id(), created_stream.id());
+
+        // list streams
+        let response = server
+            .get(format!(
+                "/api/organizations/{org_1}/projects/{proj_1}/streams"
+            ))
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let streams: Vec<Stream> = deserialize_body(response.into_body()).await;
+        assert_eq!(streams.len(), 1);
+        assert_eq!(streams[0].name, "Updated Stream");
+        assert_eq!(streams[0].id(), created_stream.id());
+
+        // remove stream
+        let response = server
+            .delete(format!(
+                "/api/organizations/{org_1}/projects/{proj_1}/streams/{}",
+                created_stream.id()
+            ))
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+
+        // verify stream is removed
+        let response = server
+            .get(format!(
+                "/api/organizations/{org_1}/projects/{proj_1}/streams"
+            ))
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let streams: Vec<Stream> = deserialize_body(response.into_body()).await;
+        assert_eq!(streams.len(), 0);
+    }
+
+    #[sqlx::test(fixtures(
+        path = "../fixtures",
+        scripts("organizations", "api_users", "projects", "streams")
+    ))]
+    async fn test_stream_no_access(pool: PgPool) {
+        let user_b = "94a98d6f-1ec0-49d2-a951-92dc0ff3042a".parse().unwrap(); // is admin of org 2
+        let org_1 = "44729d9f-a7dc-4226-b412-36a7537f5176";
+        let proj_1 = "3ba14adf-4de1-4fb6-8c20-50cc2ded5462"; // project 1 in org 1
+        let stream_1 = "85785f4c-9167-4393-bbf2-3c3e21067e4a"; // stream 1 in project 1
+        let server = TestServer::new(pool.clone(), user_b).await;
+
+        // can't list streams
+        let response = server
+            .get(format!(
+                "/api/organizations/{org_1}/projects/{proj_1}/streams"
+            ))
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::FORBIDDEN);
+
+        // can't create streams
+        let response = server
+            .post(
+                format!("/api/organizations/{org_1}/projects/{proj_1}/streams"),
+                serialize_body(NewStream {
+                    name: "Test Stream".to_string(),
+                }),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::FORBIDDEN);
+
+        // can't update stream
+        let response = server
+            .put(
+                format!("/api/organizations/{org_1}/projects/{proj_1}/streams/{stream_1}"),
+                serialize_body(NewStream {
+                    name: "Updated Stream".to_string(),
+                }),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::FORBIDDEN);
+
+        // can't delete stream
+        let response = server
+            .delete(format!(
+                "/api/organizations/{org_1}/projects/{proj_1}/streams/{stream_1}"
+            ))
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::FORBIDDEN);
+    }
+}
