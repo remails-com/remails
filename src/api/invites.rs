92a2219b19f3a9a1023976ff770365e5d9b0965b
Michiel <michielp1807@gmail.com>
2025-08-29 14:00:04 +0200

    Add additional unit tests for invites api

--- src/api/invites.rs
+++ src/api/invites.rs
@@ -83,7 +83,7 @@ pub async fn get_invite(
 
     let invite = repo.get_by_id(invite_id, org_id).await?;
     if !invite.verify_password(&password) {
-        return Err(ApiError::Forbidden);
+        return Err(ApiError::NotFound);
     }
 
     Ok(Json(invite))
@@ -122,11 +122,11 @@ pub async fn accept_invite(
     let invite = invites.get_by_id(invite_id, org_id).await?;
 
     if !invite.verify_password(&password) {
-        return Err(ApiError::Forbidden);
+        return Err(ApiError::NotFound);
     }
 
     if invite.is_expired() {
-        return Err(ApiError::Forbidden);
+        return Err(ApiError::NotFound);
     }
 
     organizations.add_user(org_id, *user.id()).await?;
@@ -134,7 +134,8 @@ pub async fn accept_invite(
     invites.remove_by_id(invite_id, org_id).await?;
 
     let Some(organization) = organizations.get_by_id(org_id).await? else {
-        return Err(ApiError::Forbidden); // this shouldn't happen
+        tracing::error!("organization not found after accepting invite: {}", org_id);
+        return Err(ApiError::NotFound); // this shouldn't happen
     };
 
     Ok((StatusCode::CREATED, Json(organization)))
@@ -145,12 +146,15 @@ mod tests {
     use axum::body::Body;
     use http::StatusCode;
     use sqlx::PgPool;
+    use tokio_util::sync::CancellationToken;
 
     use crate::{
+        HandlerConfig,
         api::{
             tests::{TestServer, deserialize_body},
             whoami::WhoamiResponse,
         },
+        handler::{Handler, dns::DnsResolver},
         models::{CreatedInviteWithPassword, OrgRole, Organization, Role},
     };
 
@@ -158,11 +162,11 @@ mod tests {
 
     #[sqlx::test(fixtures(path = "../fixtures", scripts("organizations", "api_users")))]
     async fn test_create_and_use_invite(pool: PgPool) {
-        let user_a = "9244a050-7d72-451a-9248-4b43d5108235".parse().unwrap(); // is admin of org 1 and 2
-        let user_b = "94a98d6f-1ec0-49d2-a951-92dc0ff3042a".parse().unwrap(); // is admin of org 2
-        let user_c = "54432300-128a-46a0-8a83-fe39ce3ce5ef".parse().unwrap(); // is not in any org
+        let user_1 = "9244a050-7d72-451a-9248-4b43d5108235".parse().unwrap(); // is admin of org 1 and 2
+        let user_2 = "94a98d6f-1ec0-49d2-a951-92dc0ff3042a".parse().unwrap(); // is admin of org 2
+        let user_3 = "54432300-128a-46a0-8a83-fe39ce3ce5ef".parse().unwrap(); // is not in any org
         let org_1 = "44729d9f-a7dc-4226-b412-36a7537f5176";
-        let mut server = TestServer::new(pool.clone(), Some(user_a)).await;
+        let mut server = TestServer::new(pool, Some(user_1)).await;
 
         // start with no invites
         let response = server.get(format!("/api/invite/{org_1}")).await.unwrap();
@@ -179,7 +183,7 @@ mod tests {
         let created_invite: CreatedInviteWithPassword =
             deserialize_body(response.into_body()).await;
         assert_eq!(created_invite.organization_id().to_string(), org_1);
-        assert_eq!(*created_invite.created_by(), user_a);
+        assert_eq!(*created_invite.created_by(), user_1);
 
         // new invite created
         let response = server.get(format!("/api/invite/{org_1}")).await.unwrap();
@@ -194,17 +198,10 @@ mod tests {
         );
 
         // switch to other user
-        server.set_user(Some(user_b));
+        server.set_user(Some(user_2));
 
         // can't get all invites for other organizations
         let response = server.get(format!("/api/invite/{org_1}")).await.unwrap();
-        assert_eq!(response.status(), StatusCode::FORBIDDEN);
-
-        // can't create invite for other organizations
-        let response = server
-            .post(format!("/api/invite/{org_1}"), Body::empty())
-            .await
-            .unwrap();
         assert_eq!(response.status(), StatusCode::FORBIDDEN);
 
         // get the previously create invite
@@ -212,34 +209,158 @@ mod tests {
         assert_eq!(response.status(), StatusCode::OK);
         let invite: ApiInvite = deserialize_body(response.into_body()).await;
         assert_eq!(invite.organization_id().to_string(), org_1);
-        assert_eq!(*invite.created_by(), user_a);
+        assert_eq!(*invite.created_by(), user_1);
 
-        // use invite
+        // accept invite
         let response = server.post(&invite_endpoint, Body::empty()).await.unwrap();
         assert_eq!(response.status(), StatusCode::CREATED);
         let organization: Organization = deserialize_body(response.into_body()).await;
         assert_eq!(organization.id().to_string(), org_1);
 
-        // user_b should now be an admin of org_1
+        // user_2 should now be an admin of org_1
         let response = server.get("/api/whoami").await.unwrap();
         assert_eq!(response.status(), StatusCode::OK);
         let whoami: WhoamiResponse = deserialize_body(response.into_body()).await;
-        assert_eq!(whoami.id, user_b);
+        assert_eq!(whoami.id, user_2);
         assert!(whoami.org_roles.contains(&OrgRole {
             role: Role::Admin,
             org_id: org_1.parse().unwrap()
         }));
 
-        // now user_b can see the invites of org_1
+        // now user_2 can see the invites of org_1
         let response = server.get(format!("/api/invite/{org_1}")).await.unwrap();
         assert_eq!(response.status(), StatusCode::OK);
         let invites: Vec<ApiInvite> = deserialize_body(response.into_body()).await;
         assert_eq!(invites.len(), 0); // invite was removed after use
 
-        // user_c can't get or use the same invite
-        server.set_user(Some(user_c));
+        // user_3 can't get or use the same invite
+        server.set_user(Some(user_3));
         let response = server.post(&invite_endpoint, Body::empty()).await.unwrap();
         assert_eq!(response.status(), StatusCode::NOT_FOUND);
+        let response = server.get(&invite_endpoint).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    async fn test_invites_no_access(server: &mut TestServer, status_code: StatusCode) {
+        let org_1 = "44729d9f-a7dc-4226-b412-36a7537f5176";
+        let active_invite: InviteId = "32bba198-fdd8-4cb7-8b82-85857dd2527f".parse().unwrap();
+
+        // can't get all invites for other organizations
+        let response = server.get(format!("/api/invite/{org_1}")).await.unwrap();
+        assert_eq!(response.status(), status_code);
+
+        // can't create invite for other organizations
+        let response = server
+            .post(format!("/api/invite/{org_1}"), Body::empty())
+            .await
+            .unwrap();
+        assert_eq!(response.status(), status_code);
+
+        // can't delete invite
+        let response = server
+            .delete(format!("/api/invite/{}/{}", org_1, active_invite))
+            .await
+            .unwrap();
+        assert_eq!(response.status(), status_code);
+    }
+
+    #[sqlx::test(fixtures(path = "../fixtures", scripts("organizations", "api_users", "invites")))]
+    async fn test_invites_no_access_wrong_user(pool: PgPool) {
+        let user_3 = "54432300-128a-46a0-8a83-fe39ce3ce5ef".parse().unwrap(); // is not in any org
+        let mut server = TestServer::new(pool, Some(user_3)).await;
+        test_invites_no_access(&mut server, StatusCode::FORBIDDEN).await;
+    }
+
+    #[sqlx::test(fixtures(path = "../fixtures", scripts("organizations", "api_users", "invites")))]
+    async fn test_invites_no_access_not_logged_in(pool: PgPool) {
+        let mut server = TestServer::new(pool, None).await;
+        test_invites_no_access(&mut server, StatusCode::UNAUTHORIZED).await;
+    }
+
+    #[sqlx::test(fixtures(path = "../fixtures", scripts("organizations", "api_users", "invites")))]
+    async fn test_cannot_use_removed_invite(pool: PgPool) {
+        let user_1 = "9244a050-7d72-451a-9248-4b43d5108235".parse().unwrap(); // is admin of org 1 and 2
+        let user_3 = "54432300-128a-46a0-8a83-fe39ce3ce5ef".parse().unwrap(); // is not in any org
+        let org_1 = "44729d9f-a7dc-4226-b412-36a7537f5176";
+        let active_invite: InviteId = "32bba198-fdd8-4cb7-8b82-85857dd2527f".parse().unwrap();
+        let mut server = TestServer::new(pool, None).await;
+
+        // can get removed invite
+        server.set_user(Some(user_3));
+        let invite_endpoint = format!("/api/invite/{org_1}/{}/{}", active_invite, "unsecure");
+        let response = server.get(&invite_endpoint).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+
+        // remove invite
+        server.set_user(Some(user_1));
+        let response = server
+            .delete(format!("/api/invite/{}/{}", org_1, active_invite))
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(
+            active_invite,
+            deserialize_body::<InviteId>(response.into_body()).await
+        );
+
+        // can't get removed invite
+        server.set_user(Some(user_3));
+        let invite_endpoint = format!("/api/invite/{org_1}/{}/{}", active_invite, "unsecure");
+        let response = server.get(&invite_endpoint).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+        // can't accept removed invite
+        let response = server.post(&invite_endpoint, Body::empty()).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[sqlx::test(fixtures(path = "../fixtures", scripts("organizations", "api_users", "invites")))]
+    async fn test_wrong_invite_password(pool: PgPool) {
+        let user_3 = "54432300-128a-46a0-8a83-fe39ce3ce5ef".parse().unwrap(); // is not in any org
+        let org_1 = "44729d9f-a7dc-4226-b412-36a7537f5176";
+        let active_invite: InviteId = "32bba198-fdd8-4cb7-8b82-85857dd2527f".parse().unwrap();
+        let server = TestServer::new(pool, Some(user_3)).await;
+
+        let invite_endpoint = format!("/api/invite/{org_1}/{}/{}", active_invite, "wrong-password");
+
+        // can't get invite data with wrong password
+        let response = server.get(&invite_endpoint).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+        // can't accept invite with wrong password
+        let response = server.post(&invite_endpoint, Body::empty()).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[sqlx::test(fixtures(path = "../fixtures", scripts("organizations", "api_users", "invites")))]
+    async fn test_expired_invite(pool: PgPool) {
+        let user_3 = "54432300-128a-46a0-8a83-fe39ce3ce5ef".parse().unwrap(); // is not in any org
+        let org_1 = "44729d9f-a7dc-4226-b412-36a7537f5176";
+        let expired_invite: InviteId = "8b01ce56-4304-47c7-b9a6-62bd1b7e8269".parse().unwrap();
+        let server = TestServer::new(pool.clone(), Some(user_3)).await;
+
+        let invite_endpoint = format!("/api/invite/{org_1}/{}/{}", expired_invite, "unsecure");
+
+        // can get expired invite
+        let response = server.get(&invite_endpoint).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+
+        // can't accept expired invite
+        let response = server.post(&invite_endpoint, Body::empty()).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+        // expired invite will be removed eventually
+        let config = HandlerConfig {
+            allow_plain: true,
+            domain: "test".to_string(),
+            resolver: DnsResolver::mock("localhost", 0),
+            retry_delay: chrono::Duration::minutes(5),
+            max_automatic_retries: 3,
+        };
+        let message_handler = Handler::new(pool.clone(), config.into(), CancellationToken::new());
+        message_handler.periodic_clean_up().await.unwrap();
+
+        // cannot get automatically removed expired invite
         let response = server.get(&invite_endpoint).await.unwrap();
         assert_eq!(response.status(), StatusCode::NOT_FOUND);
     }
---
+++ src/fixtures/invites.sql
@@ -0,0 +1,11 @@
+INSERT INTO organization_invites (id, organization_id, created_by, password_hash, expires_at)
+VALUES ('32bba198-fdd8-4cb7-8b82-85857dd2527f',
+        '44729d9f-a7dc-4226-b412-36a7537f5176', -- org 1
+        '9244a050-7d72-451a-9248-4b43d5108235', -- test user 1
+        '$argon2id$v=19$m=16,t=2,p=1$WWJQYkFxb0lXc0JkSHFDTw$4BYwZvcePs/l2WewT+TpcQ', -- password is unsecure
+        now() + INTERVAL '7d'),
+       ('8b01ce56-4304-47c7-b9a6-62bd1b7e8269',
+        '44729d9f-a7dc-4226-b412-36a7537f5176', -- org 1
+        '9244a050-7d72-451a-9248-4b43d5108235', -- test user 1
+        '$argon2id$v=19$m=16,t=2,p=1$WWJQYkFxb0lXc0JkSHFDTw$4BYwZvcePs/l2WewT+TpcQ', -- password is unsecure
+        now() - INTERVAL '1d'); -- expired invite
