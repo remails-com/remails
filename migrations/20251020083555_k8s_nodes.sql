fab5ff67a5b32cb0b1f6e2b999a278e3ff471822
Maximilian Pohl <maximilian@tweedegolf.com>
2025-10-27 10:20:58 +0100

    Adopt migration

--- migrations/20251020083555_k8s_nodes.sql
+++ migrations/20251020083555_k8s_nodes.sql
@@ -10,7 +10,7 @@ CREATE TABLE outbound_ips
 (
     id      uuid NOT NULL PRIMARY KEY,
     ip      inet NOT NULL UNIQUE,
-    node_id uuid REFERENCES k8s_nodes (id)
+    node_id uuid REFERENCES k8s_nodes (id) ON DELETE SET NULL
 );
 
 ALTER TABLE messages
