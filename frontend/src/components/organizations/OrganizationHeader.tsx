b9259ff52bc04c4c8a58747cc6e02021441f3968
Michiel <michielp1807@gmail.com>
2025-11-05 16:10:59 +0100

    Add warning in front-end for blocked organizations

--- frontend/src/components/organizations/OrganizationHeader.tsx
+++ frontend/src/components/organizations/OrganizationHeader.tsx
@@ -1,9 +1,34 @@
-import { IconBuildings } from "@tabler/icons-react";
+import { IconAlertTriangle, IconBuildings } from "@tabler/icons-react";
 import { useOrganizations } from "../../hooks/useOrganizations";
 import Header from "../Header";
+import { ThemeIcon, Tooltip } from "@mantine/core";
 
 export default function OrganizationHeader() {
   const { currentOrganization } = useOrganizations();
 
-  return <Header name={currentOrganization?.name ?? ""} entityType={"Organization"} Icon={IconBuildings} divider />;
+  let blocked_warning = null;
+
+  if (currentOrganization?.block_status != "not_blocked") {
+    let warning = "This organization has been blocked by the Remails admins from sending emails";
+    if (currentOrganization?.block_status == "no_sending_or_receiving") {
+      warning = "This organization has been blocked by the Remails admins from sending and receiving emails";
+    }
+    blocked_warning = (
+      <Tooltip label={warning}>
+        <ThemeIcon variant="transparent">
+          <IconAlertTriangle />
+        </ThemeIcon>
+      </Tooltip>
+    );
+  }
+
+  return (
+    <Header
+      name={currentOrganization?.name ?? ""}
+      entityType={"Organization"}
+      Icon={IconBuildings}
+      divider
+      addendum={blocked_warning}
+    />
+  );
 }
