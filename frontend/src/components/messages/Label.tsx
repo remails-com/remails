0b092a7def1ead2a54d9c58b27afa2a41f51ce4f
Michiel <michielp1807@gmail.com>
2025-11-18 17:28:53 +0100

    Randomize label color

--- frontend/src/components/messages/Label.tsx
+++ frontend/src/components/messages/Label.tsx
@@ -1,6 +1,14 @@
 import { Badge } from "@mantine/core";
 import { useRemails } from "../../hooks/useRemails.ts";
 
+function randomNumberFromStr(str: string): number {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash * 31 + str.charCodeAt(i)) | 0;
+  }
+  return hash >>> 0; // unsigned
+}
+
 export default function Label({ label }: { label: string }) {
   const { navigate } = useRemails();
 
@@ -9,7 +17,7 @@ export default function Label({ label }: { label: string }) {
       style={{
         cursor: "pointer",
       }}
-      color="#FF246B"
+      color={`hsl(${randomNumberFromStr(label) % 360}, 68%, 34%)`}
       onClick={(e) => {
         e.stopPropagation();
         navigate("projects.project.messages", {
