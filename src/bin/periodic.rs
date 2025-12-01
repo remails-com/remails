3dd474e1984c20ede5e6539f994296e1179aa7bc
Maximilian Pohl <maximilian@tweedegolf.com>
2025-12-01 10:19:37 +0100

    Add livenessProbe to periodic container

--- src/bin/periodic.rs
+++ src/bin/periodic.rs
@@ -6,6 +6,7 @@ use sqlx::{
     ConnectOptions,
     postgres::{PgConnectOptions, PgPoolOptions},
 };
+use std::{fs::OpenOptions, path::Path, time::SystemTime};
 use tokio::time::{self, Duration};
 use tokio_util::sync::CancellationToken;
 use tracing::{error, info, warn};
@@ -35,11 +36,11 @@ async fn main() -> anyhow::Result<()> {
     let kubernetes = Kubernetes::new(pool.clone()).await?;
 
     let shutdown = CancellationToken::new();
-    let mut chech_nodes_interval = time::interval(Duration::from_secs(10)); // Every 30 seconds
+    let mut check_nodes_interval = time::interval(Duration::from_secs(10)); // Every 10 seconds
     let mut message_retry_interval = time::interval(Duration::from_secs(60)); // Every minute
     let mut reset_all_quotas_interval = time::interval(Duration::from_secs(10 * 60)); // Every 10 minutes
     let mut clean_up_invites_interval = time::interval(Duration::from_secs(4 * 60 * 60)); // Every 4 hours
-    chech_nodes_interval.set_missed_tick_behavior(time::MissedTickBehavior::Delay);
+    check_nodes_interval.set_missed_tick_behavior(time::MissedTickBehavior::Delay);
     message_retry_interval.set_missed_tick_behavior(time::MissedTickBehavior::Delay);
     reset_all_quotas_interval.set_missed_tick_behavior(time::MissedTickBehavior::Delay);
     clean_up_invites_interval.set_missed_tick_behavior(time::MissedTickBehavior::Delay);
@@ -49,24 +50,32 @@ async fn main() -> anyhow::Result<()> {
     let join_handle = tokio::spawn(async move {
         loop {
             tokio::select! {
-                _ = chech_nodes_interval.tick() => {
+                _ = check_nodes_interval.tick() => {
                     if let Err(err) = kubernetes.check_node_health().await {
                         error!("Failed to check K8s nodes health: {}", err);
+                    } else {
+                        update_healthcheck("check_node_health")
                     }
                 },
                 _ = message_retry_interval.tick() => {
                     if let Err(err) = periodically.retry_messages().await {
                         error!("Failed to retry messages: {}", err);
+                    } else {
+                        update_healthcheck("retry_messages")
                     }
                 },
                 _ = reset_all_quotas_interval.tick() => {
                     if let Err(err) = periodically.reset_all_quotas().await {
                         error!("Failed to reset all quotas: {}", err);
+                    } else {
+                        update_healthcheck("reset_all_quotas")
                     }
                 },
                 _ = clean_up_invites_interval.tick() =>  {
                     if let Err(err) = periodically.clean_up_invites().await {
                         error!("Failed to clean up invites: {}", err);
+                    } else {
+                        update_healthcheck("clean_up_invites")
                     }
                 },
                 _ = shutdown_clone.cancelled() => {
@@ -92,4 +101,24 @@ async fn main() -> anyhow::Result<()> {
     );
 
     Ok(())
+}
+
+/// On every call, this function updates the modification timestamp on the specified file.
+/// Kubernetes then checks that this timestamp of the file in question is not too old, which would
+/// indicate something like a deadlock or other sort of failure.
+/// If a failure is detected, Kubernetes will restart the container.
+fn update_healthcheck(name: &'static str) {
+    let mut path = Path::new("/tmp").join(name);
+    path.add_extension("healthcheck");
+    if let Ok(file) = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(path)
+        .inspect_err(|err| error!("Failed to update healthcheck file: {err}"))
+    {
+        file.set_modified(SystemTime::now())
+            .inspect_err(|err| error!("Failed to update healthcheck file: {err}"))
+            .ok();
+    }
 }
