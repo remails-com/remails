727e32d44e89b20896fef233b721da8132ded6e5
Maximilian Pohl <maximilian@tweedegolf.com>
2025-12-01 10:42:29 +0100

    Add livenessProbe to message bus

--- src/bus/server.rs
+++ src/bus/server.rs
@@ -5,8 +5,12 @@ use axum::{
     routing::{get, post},
 };
 use http::StatusCode;
+use humansize::ToF64;
 use std::net::SocketAddrV4;
 use tokio::sync::broadcast;
+use tracing::{error, trace, warn};
+
+pub const CAPACITY: usize = 128;
 
 #[derive(Clone)]
 struct BusState {
@@ -23,6 +27,7 @@ impl Bus {
         let router = Router::new()
             .route("/listen", get(ws_handler))
             .route("/post", post(new_message))
+            .route("/healthy", get(healthy))
             .with_state(BusState { message_tx });
 
         Bus { router, socket }
@@ -49,7 +54,7 @@ impl Bus {
         let bus_port = rand::Rng::random_range(&mut rng, 10_000..30_000);
         let bus_socket = SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), bus_port);
 
-        let (tx, _rx) = tokio::sync::broadcast::channel::<String>(100);
+        let tx = broadcast::Sender::<String>::new(CAPACITY);
         let bus = Bus::new(bus_socket, tx);
         tokio::spawn(async { bus.serve().await });
 
@@ -61,11 +66,11 @@ async fn new_message(State(state): State<BusState>, body: String) -> impl IntoRe
     tracing::info!("new message: {body}");
     match state.message_tx.send(body) {
         Ok(n) => {
-            tracing::trace!("sent message to {n} listeners");
+            trace!("sent message to {n} listeners");
             (StatusCode::ACCEPTED, format!("{n}"))
         }
         Err(e) => {
-            tracing::error!("error sending message (probably no active subscribers): {e}");
+            error!("error sending message (probably no active subscribers): {e}");
             (StatusCode::ACCEPTED, "0".to_string())
         }
     }
@@ -77,8 +82,23 @@ async fn ws_handler(ws: WebSocketUpgrade, State(state): State<BusState>) -> impl
     ws.on_upgrade(|mut socket| async move {
         while let Ok(message) = messages_rx.recv().await {
             if let Err(e) = socket.send(Message::Text(message.into())).await {
-                tracing::error!("Error sending WS message: {e}");
+                error!("Error sending WS message: {e}");
             }
         }
     })
+}
+
+async fn healthy(State(state): State<BusState>) -> StatusCode {
+    trace!("called /healthy");
+
+    if state.message_tx.len().to_f64() > CAPACITY.to_f64() * 0.7 {
+        warn!(
+            receiver_count = state.message_tx.receiver_count(),
+            "Message bus has a huge backlog ({} of max {})",
+            state.message_tx.len(),
+            CAPACITY
+        )
+    }
+
+    StatusCode::OK
 }
